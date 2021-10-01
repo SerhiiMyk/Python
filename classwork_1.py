c219e744c3aaf1fdb29d380a4da67fd257981d7d
SerhiiMyk <sergey.mykolaichuk@gmail.com>
2021-10-01 18:37:23 +0300

    done

--- classwork_1.py
+++ classwork_1.py
@@ -10,31 +10,46 @@
 #     '9) Выход'
 # - можете придумать свои пункты
 
+
+notebook = dict()
+price_list=[]
+
 j=1
 while j!=0:
-    print('1) Создать запись\n'
+    print('\n1) Создать запись\n'
           '2) Список все записей\n'
           '3) Общая сумма всех покупок\n'
           '4) Самая дорогая покупка\n'
           '5) Поиск по названию покупки\n'
-          '6) Выход\n')
-    select_item = int(input('\nselect a menu item:'))
-
-    notebook = dict()
+          '6) Выход')
+    select_item = int(input('\nselect a menu item: '))
     if select_item == 1:
         i=1
         while i!=0:
-            p_name = input('\nwrite name of the product:')
-            p_price = int(input('write price of the product:'))
+            p_name = input('\nwrite name of the product: ')
+            p_price = int(input('write price of the product: '))
             notebook.update({p_name:p_price})
-            print('\n', notebook, '\n')
-            i=int(input('if you want to add one more item press 1 or if you want to exit press 0:'))
-
+            # print('\n', notebook, '\n')
+            i=int(input('if you want to add one more item press 1 or if you want to exit press 0: '))
+            if i!=1 and i!=0:
+                while i!=1 or i!=0:
+                    i = int(input('\nPlease press 1 or 0: '))
+                    if i==1 or i==0:
+                        break
     elif select_item==2:
-
         for k,v in notebook.items():
-            print(k,v,sep='-')
-
+            print('\n',k,' - ',v,'UAH')
+            price_list.append(v)
+        print()
+    elif select_item == 3:
+        print('\nThe total amount of all purchases: ', sum(price_list),'UAH')
+    elif select_item == 4:
+        print('The most expensive purchase: ', max(price_list),'UAH')
+    elif select_item == 5:
+        search=input('\nenter the product name: ')
+        for k,v in notebook.items():
+            if search==k:
+                print('\nproduct cost: ',v,'UAH')
     elif select_item==6:
         j=0
 
