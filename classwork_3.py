121b2fc737c85cbe9290001558924ab211320284
SerhiiMyk <sergey.mykolaichuk@gmail.com>
2021-10-08 00:34:19 +0300

    done

---
+++ classwork_3.py
@@ -0,0 +1,90 @@
+# реализовать записную книжку покупок:
+# - каждая запись должна содержать название покупки и ее цену
+# -сделать менюшку для работы с записной книжкой:
+# -каждый пункт меню должен быть методом класса
+#     '1) Создать запись'
+#     '2) Список все записей'
+#     '3) Общая сумма всех покупок'
+#     '4) Самая дорогая покупка'
+#     '5) Поиск по названию покупки'
+#     '9) Выход'
+# - можете придумать свои пункты
+
+
+class Product:
+
+    def __init__(self, product, price):
+        self.product = product
+        self.price = price
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+class Notebook:
+    price_list = []
+
+    def __init__(self):
+        self.__product_list: list[Product] = []
+
+    def create(self):
+        product = input('\nwrite name ot the product: ')
+        price = input('write price: ')
+        self.__product_list.append(Product(product, price))
+
+    def show_all(self):
+        for i in self.__product_list:
+            print(i.product, ' - ', i.price, 'UAH')
+
+    def sum(self):
+
+        for i in self.__product_list:
+            self.price_list.append(int(i.price))
+        return sum(self.price_list)
+
+    def max(self):
+
+        for i in self.__product_list:
+            self.price_list.append(int(i.price))
+        return max(self.price_list)
+
+    def search(self):
+        search = input('enter the product name: ')
+        for i in self.__product_list:
+            if search == i.product:
+                return i.price
+
+
+notebook = Notebook()
+j = 1
+while j != 0:
+    print('\n1) Создать запись\n'
+          '2) Список все записей\n'
+          '3) Общая сумма всех покупок\n'
+          '4) Самая дорогая покупка\n'
+          '5) Поиск по названию покупки\n'
+          '6) Выход')
+    select_item = int(input('\nselect a menu item: '))
+    print()
+    if select_item == 1:
+        i = 1
+        while i != 0:
+            notebook.create()
+            i = int(input('\nif you want to add one more item press 1 or if you want to exit press 0: '))
+            if i != 1 and i != 0:
+                while i != 1 or i != 0:
+                    i = int(input('\nPlease press 1 or 0: '))
+                    if i == 1 or i == 0:
+                        break
+    elif select_item == 2:
+        notebook.show_all()
+    elif select_item == 3:
+        print('The total amount of all purchases: ', notebook.sum(), 'UAH')
+    elif select_item == 4:
+        print('The most expensive purchase: ', notebook.max(), 'UAH')
+    elif select_item == 5:
+        print('\nProduct cost: ', notebook.search(), 'UAH')
+    elif select_item == 6:
+        j = 0
+    else:
+        print('Please write a correct number of submenu!')
--- homework_4.py
+++ homework_4.py
@@ -51,6 +51,41 @@
 # 6) має бути метод(метод класу) для виводу __сount
 # 7) має бути метод який записує в наш ліст текст з нашої змінної __text
 
+# При створенні об'єкта. Мається на увазі конструктор?
+
+# list = []
+#
+# class Latter:
+#     __count = 0
+#
+#     def __init__(self):
+#         self.__text = None
+#         Latter.__count += 1
+#
+#     @property
+#     def text(self):
+#         return self.__text
+#
+#     @text.setter
+#     def text(self, new_value):
+#         self.__text = new_value
+#
+#     @classmethod
+#     def count(cls):
+#         return cls.__count
+#
+#     def add(self):
+#         list.append(self.__text)
+#
+#
+# letter1 = Latter()
+#
+# letter1.text = 'hi'
+# print(letter1.text)
+#
+# letter1.add()
+#
+# print(list)
 
 # создать класс Human (name, age)
 # создать два класса Prince и Cinderella:
@@ -59,3 +94,51 @@
 #
 # класса золушки должна быть переменная count которая будет считать сколько экземпляров класса золушка было создано
 # и метод класса который будет показывать это количество
+
+# class Human:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#
+# class Cinderella(Human):
+#     count = 0
+#
+#     def __init__(self, name, age, size):
+#         super().__init__(name, age)
+#         self.size = size
+#         Cinderella.count += 1
+#
+#     def __str__(self):
+#         return str(self.__dict__)
+#
+#     def num(self):
+#         return self.count
+#
+#
+# class Prince(Human):
+#     def __init__(self, name, age, search):
+#         super().__init__(name, age)
+#         self.search = search
+#
+#     def __str__(self):
+#         return str(self.__dict__)
+#
+#     def cind_list(self, c_list):
+#
+#         for i in range(len(c_list)):
+#             if c_list[i].size == self.search:
+#                 return c_list[i].name
+#
+#
+# c_list = [Cinderella('Alina', '19', 36),
+#           Cinderella('Dasha', '20', 37),
+#           Cinderella('Masha', '21', 35)]
+#
+# prince = Prince('Patrik', '25', 35)
+#
+# print(prince)
+#
+# print(prince.cind_list(c_list))
+#
+# print(Cinderella.count)
